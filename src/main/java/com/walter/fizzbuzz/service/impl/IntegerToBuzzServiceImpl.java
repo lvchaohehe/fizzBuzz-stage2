3856258c1eb385a2d68e4867d1fb45fe2f713948
Bill <lvchao335@163.com>
2019-04-18 00:44:26 +0800

    Jenkinsfile

---
+++ src/main/java/com/walter/fizzbuzz/service/impl/IntegerToBuzzServiceImpl.java
@@ -0,0 +1,20 @@
+package com.walter.fizzbuzz.service.impl;
+
+import com.walter.fizzbuzz.constant.Constant;
+import com.walter.fizzbuzz.service.IntegerToStringService;
+
+/**
+ * IntegerToStringService implementation for BUZZ case.
+ * @author hp
+ */
+public class IntegerToBuzzServiceImpl implements IntegerToStringService {
+
+	public String transfer(int n) {
+		return Constant.BUZZ;
+	}
+
+	public boolean support(int n) {
+		return 0 == n%5 || String.valueOf(n).contains("5");
+	}
+
+}
---
+++ src/main/java/com/walter/fizzbuzz/service/impl/IntegerToFizzServiceImpl.java
@@ -0,0 +1,20 @@
+package com.walter.fizzbuzz.service.impl;
+
+import com.walter.fizzbuzz.constant.Constant;
+import com.walter.fizzbuzz.service.IntegerToStringService;
+
+/**
+ * IntegerToStringService implementation for FIZZ case.
+ * @author hp
+ */
+public class IntegerToFizzServiceImpl implements IntegerToStringService {
+
+	public String transfer(int n) {
+		return Constant.FIZZ;
+	}
+
+	public boolean support(int n) {
+		return 0 == n%3 || String.valueOf(n).contains("3");
+	}
+
+}
